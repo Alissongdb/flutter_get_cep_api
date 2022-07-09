b88480eab140059a337c6f4f475eca929b137b73
Alissongdb <alisson.gabriel5@hotmail.com>
2022-07-09 17:21:11 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/with_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.with_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/zip_code.dart
@@ -0,0 +1,15 @@
+class ZipCode {
+  String street;
+  String district;
+  String number;
+  String city;
+  String uf;
+
+  ZipCode({
+    required this.street,
+    required this.district,
+    required this.number,
+    required this.city,
+    required this.uf,
+  });
+}
